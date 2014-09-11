d456f36a502c28c3eeaed18edb79f74f9c59b8bc
Torsten Braun <tbraun@tnt-web-solutions.de>
2014-09-11 11:48:53 +0200

    init gem

---
+++ lib/ruby/advisory/db/check.rb
@@ -0,0 +1,11 @@
+require "ruby/advisory/db/check/version"
+
+module Ruby
+  module Advisory
+    module Db
+      module Check
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/ruby/advisory/db/check/version.rb
@@ -0,0 +1,9 @@
+module Ruby
+  module Advisory
+    module Db
+      module Check
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
